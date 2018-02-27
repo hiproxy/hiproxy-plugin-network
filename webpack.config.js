149ae786ab8fd4c88b93ab8eb3fa06ee0e9e7c56
fenghua.zhang <fenghua.zhang@qunar.com>
2018-02-27 14:33:37 +0800

    减小size

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ module.exports = {
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     }),
     process.env.NODE_ENV === 'production' ? new UglifyJsPlugin() : function () {},
-    new BundleAnalyzerPlugin()
+    //new BundleAnalyzerPlugin()
   ],
 
   resolve: {
